94698517ec95d36d84be6e449c9219a783255cbb
Brian Wakhale <brianweke1998@gmail.com>
2025-03-15 20:10:07 +0300

    feat: Add Get Started button to index.js

--- notes-app-25/app/index.jsx
+++ notes-app-25/app/index.jsx
@@ -1,10 +1,19 @@
-import { Text, View,StyleSheet,Image } from "react-native";
+import { Text, View,StyleSheet,Image,TouchableOpacity } from "react-native";
 import PostItImage from '@/assets/images/post-it.png';
+import { router, useRouter } from "expo-router";
  const HomeScreen=()=> {
   return (
     <View style={styles.container}>
       <Image source={PostItImage} style={styles.Image} />
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.title}>Welcome To Notes App.</Text>
+      <Text style={styles.subtitle}>Never Miss a Thought, Anytime, Anywhere.</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={()=> router.push('/notes')}
+      >
+        <Text style={styles.buttonText}>Get Started</Text>
+       
+      </TouchableOpacity>
     </View>
   );
 }
@@ -21,6 +30,30 @@ const styles = StyleSheet.create({
     height: 200,
     marginBottom: 20,
     borderRadius: 10,
-  }
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+    button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 })
 export default HomeScreen;
