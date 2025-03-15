f580a0c80f385c3d01169acf75bba788b3acc93a
Brian Wakhale <brianweke1998@gmail.com>
2025-03-15 18:28:38 +0300

    feat:implement images in assets directory and post it image

--- notes-app-25/app/index.jsx
+++ notes-app-25/app/index.jsx
@@ -1,8 +1,9 @@
-import { Text, View,StyleSheet } from "react-native";
-
+import { Text, View,StyleSheet,Image } from "react-native";
+import PostItImage from '@/assets/images/post-it.png';
  const HomeScreen=()=> {
   return (
     <View style={styles.container}>
+      <Image source={PostItImage} style={styles.Image} />
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
@@ -11,7 +12,15 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     justifyContent:'center',
-    alignItems:'center'
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor:'#f8f9fa'
+  },
+  Image:{
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+    borderRadius: 10,
   }
 })
 export default HomeScreen;
